f5d8c628e21ae325aa69d3277381534b6410b23a
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-11-20 00:11:18 -0700

    grid working

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
+let size = 20;
+let cols, rows;
+let margin = 30;
+
 function setup() {
 	createCanvas(400, 400, WEBGL);
+	cols = (width - margin * 2) / size;
+	rows = (height - margin * 2) / size;
 }
 
 function draw() {
 	background(220);
 	orbitControl();
 
-	translate(0, 0, 0);
-	box(50);
-
-	translate(50, 0, 0);
-	box(50);
-
-	translate(0, 50, 0);
-	box(50);
-
-	translate(50, 50, 0);
-	box(50);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			push();
+			let x = -width / 2 + margin + size / 2 + i * size;
+			let y = -height / 2 + margin + size / 2 + j * size;
+			let z = 0;
+			translate(x, y, z);
+			box(size);
+			pop();
+		}
+	}
 }
